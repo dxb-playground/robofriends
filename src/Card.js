94fcedc1ab8a3a7689cfa148ff4827c5f8664f41
Peter Cortez <pcortez@crmonline.com.au>
2020-07-31 14:01:31 +0800

    Section 74

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Card = ({ id, name, email }) => {
   return (
     <div className='bg-light-green dib br3 pa3 ma2 grow'>
-      <h1>RoboFriends</h1>
       <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
       <div>
         <h2>{name}</h2>
