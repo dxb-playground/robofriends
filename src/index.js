5e8a37b7d9282b5b32d8c35fac9be23cb15a3043
Peter Cortez <pcortez@crmonline.com.au>
2020-08-04 16:24:27 +0800

    Section 89

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
