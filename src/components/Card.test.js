5ad5d571aa678a6b7a5ae7e5bd4acba6e0377fbb
Peter Cortez <pcortez@crmonline.com.au>
2020-08-11 17:43:57 +0800

    Section 6: 161

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('expects to render the Card component', () => {
   let component = shallow(<Card />);
 
-  expect(component.length).toEqual(1);
+  expect(component).toMatchSnapshot();
 });
